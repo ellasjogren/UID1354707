d60f121c10f4b159c33c4e08df377a774fd45481
ellasjogren <164464170+ellasjogren@users.noreply.github.com>
2024-05-09 17:07:31 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function search_website(event) {
+    event.preventDefault(); // Prevent the default form submission action
+    let input = document.getElementById('searchbar').value.toLowerCase();
+    let x = document.querySelectorAll('.Website'); // Use querySelectorAll for better performance
+    
+    x.forEach(item => {
+        let itemName = item.textContent.toLowerCase();
+        if (itemName.includes(input)) {
+            item.style.display = "list-item";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
