6740be949c4d9c6cc219dea388d45ba216230fe2
ellasjogren <164464170+ellasjogren@users.noreply.github.com>
2024-05-15 14:33:58 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-function search_website(event) {
-    event.preventDefault(); // Prevent the default form submission action
-    let input = document.getElementById('searchbar').value.toLowerCase();
-    let x = document.querySelectorAll('.Website'); // Use querySelectorAll for better performance
-    
-    x.forEach(item => {
-        let itemName = item.textContent.toLowerCase();
-        if (itemName.includes(input)) {
-            item.style.display = "list-item";
-        } else {
-            item.style.display = "none";
-        }
-    });
+
+// CART
+function showCart() {
+    var cartPopup = document.getElementById("cartPopup");
+    if (cartPopup.style.display === "none" || cartPopup.style.display === "") {
+        cartPopup.style.display = "block";
+    } else {
+        cartPopup.style.display = "none";
+    }
 }
-document.getElementById('add-to-cart').addEventListener('click', function() {
-    var quantity = document.getElementById('quantity').value;
-    alert('Added ' + quantity + ' to cart!');
-  });
-  
